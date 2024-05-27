7e03c3aff6cc68a4ea1c08abb739df17e03e9d46
adelowomi <adelowomi@gmail.com>
2024-05-27 14:38:52 +0100

    updated required fields for ticket model

--- NGOAPP/Models/InputModels/EventTicketModel.cs
+++ NGOAPP/Models/InputModels/EventTicketModel.cs
@@ -4,13 +4,13 @@ public class EventTicketModel
 {
     public Guid EventId { get; set; }
     public string Name { get; set; }
-    public string Description { get; set; }
+    public string? Description { get; set; }
     public int MaxTicketPerOrder { get; set; }
     public decimal Price { get; set; }
     public decimal Discount { get; set; }
     public decimal Total { get; set; }
     public int Quantity { get; set; }
-    public string ImageUrl { get; set; }
+    public string? ImageUrl { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public bool IsAvailable { get; set; }
