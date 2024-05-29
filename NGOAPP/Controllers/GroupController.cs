5f3e5f877d4f7ffd90a6bad9490f834cc4bb8c59
adelowomi <adelowomi@gmail.com>
2024-05-29 11:24:46 +0100

    updated group controller to have authorization

--- NGOAPP/Controllers/GroupController.cs
+++ NGOAPP/Controllers/GroupController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace NGOAPP;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class GroupController : StandardControllerBase
 {
     private readonly IGroupService _groupService;
